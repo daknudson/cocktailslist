2c1296f12c726d66ab5bb556e4a098491704ec25
Dayne Knudson <buckycucky@gmail.com>
2022-08-02 10:19:48 -0600

    cocktail party

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <section className="section about-section">
+    <h1 className='section-title'>About Us</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, nulla, consequatur accusamus iste provident illum deserunt, reprehenderit dolorum dolorem reiciendis unde. Eveniet, impedit id fuga possimus minus fugit, at veniam ducimus reprehenderit tempora, maxime sequi hic dignissimos officiis aliquid nulla?</p>
+   </section>
+  )
+}
+
+export default About
